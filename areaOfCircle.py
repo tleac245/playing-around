40fb7b5abed1b78d50b6a97afeb862369f77be6c
Taylor Leach <taylorwleach@gmail.com>
2023-04-23 14:30:07 -0700

    Added some SQLite db connection code that I was trying to figure out.

--- areaOfCircle.py
+++ areaOfCircle.py
@@ -1,5 +1,7 @@
 import numpy
 import circumference
+import sqlite3
+import os
 
 print("Calculating the area  and circumference of a circle.")
 
@@ -8,3 +10,15 @@ radius = float(input("Enter the radius length: "))
 area = numpy.pi * radius**2
 print("The area is: ", area)
 print("The circumference is: ", circumference.calcCircum(radius))
+
+myPath = r'C:\Users\Taylor\AppData\Roaming\DBeaverData\workspace6\.metadata\sample-database-sqlite-1'
+myFile = "Chinook.db"
+
+
+#conn = sqlite3.connect(os.path.join(myPath, myFile))
+conn = sqlite3.connect(r"C:\Users\Taylor\AppData\Roaming\DBeaverData\workspace6\.metadata\sample-database-sqlite-1\Chinook.db")
+
+cur = conn.cursor()
+#cur.execute("CREATE TABLE movie(title, year, score);")
+res = cur.execute("SELECT name FROM sqlite_master;")
+print(res.fetchall())
